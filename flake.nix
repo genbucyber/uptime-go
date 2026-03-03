9623773dd9db59b7c31baf11ba845f9a0008fdfa
Aspian <muhammad.aspian.d@gmail.com>
2026-03-03 22:48:04 +0800

    chore: package app with Nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Go development environment for uptime-go";
+  description = "Uptime monitoring service written in Go";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05-small";
@@ -31,6 +31,25 @@
           shellHooks = ''
             export GIN_MODE=debug
           '';
+        };
+
+        packages.default = pkgs.buildGoModule {
+          pname = "uptime-go";
+          version = "0.4.3";
+          src = self;
+          vendorHash = "sha256-p11U7GGDRO/FiKIZQ98i7lJ98MJ93Vo84x9+FHfqueA=";
+
+          meta = {
+            description = "Uptime monitoring service written in Go";
+            homepage = "https://github.com/genbucyber/uptime-go";
+            maintainers = with pkgs.lib.maintainers; [ Aspiand ];
+            platforms = pkgs.lib.platforms.all;
+          };
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/uptime-go";
         };
       }
     );
